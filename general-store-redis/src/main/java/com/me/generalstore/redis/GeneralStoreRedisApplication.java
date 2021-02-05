a18fc1f06cb52da448fdf0501f490fe8b3d5ef77
dingqaing <dingqiangwork@163.com>
2021-02-05 16:12:32 +0800

    结构改造

---
+++ general-store-redis/src/main/java/com/me/generalstore/redis/GeneralStoreRedisApplication.java
@@ -0,0 +1,13 @@
+package com.me.generalstore.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralStoreRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralStoreRedisApplication.class, args);
+    }
+
+}
---
+++ general-store-redis/src/test/java/com/me/generalstore/redis/GeneralStoreRedisApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.me.generalstore.redis;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GeneralStoreRedisApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
